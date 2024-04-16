ef8d46fc3f59871b7c6ecf926ef32bf0a56a2585
Darwing <darwingdiazsanchez@gmail.com>
2024-04-16 18:23:11 -0500

    detail

--- chefCompass/src/Views/Detail/Detail.jsx
+++ chefCompass/src/Views/Detail/Detail.jsx
@@ -1,9 +1,18 @@
 const Detail = () => {
 
   return(
-    <>
-      <h1>Este es el Detail</h1>
-    </>
+    <div className="container d-flex mt-5">
+      <div className="container">
+
+      </div>
+      <div className="container">
+        <img src="https://w6h5a5r4.rocketcdn.me/wp-content/uploads/2019/06/pizza-con-chorizo-jamon-y-queso-1080x671.jpg" alt="" class="img-thumbnail img-fluid"/>
+        <h1>Pizza Napolitana</h1>
+        <h5>Duration:</h5>
+        <h2>Ingredients:</h2>
+        <h2>Preparation:</h2>
+      </div> 
+    </div>
   )
 }
 
